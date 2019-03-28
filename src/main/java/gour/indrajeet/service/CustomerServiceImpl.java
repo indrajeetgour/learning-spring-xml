ad38255402e362a76aab1a25c3340fb66714b0cf
IGour <ijgour@gmail.com>
2019-03-28 17:42:08 +0530

    implemented setter injection

--- src/main/java/gour/indrajeet/service/CustomerServiceImpl.java
+++ src/main/java/gour/indrajeet/service/CustomerServiceImpl.java
@@ -2,19 +2,23 @@ package gour.indrajeet.service;
 
 import gour.indrajeet.model.Customer;
 import gour.indrajeet.repository.CustomerRepository;
-import gour.indrajeet.repository.HibernateCustomerRepositoryImpl;
 
 import java.util.List;
 
 public class CustomerServiceImpl implements CustomerService {
     // here we will have all the service logic on service tier
 
-    //    custoomer repo implementation
-    private CustomerRepository customerRepository = new HibernateCustomerRepositoryImpl();
+    // customer repo implementation
+    private CustomerRepository customerRepository;
 
-    //    simple pass through method
+    // simple pass through method
     public List<Customer> findAll() {
         return customerRepository.findAll();
+    }
+
+    // constructor injection
+    public void setCustomerRepository(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
     }
 
 }
